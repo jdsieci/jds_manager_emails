8ec33230ba193aa6c76055acbb890d4087d5bcaa
Jerzy Drozdz <jerzy.drozdz@jdsieci.pl>
2013-02-25 12:54:02 +0100

    email_quota tables added

---
+++ src/model/email_quota.erl
@@ -0,0 +1,28 @@
+%% Author: tofik
+%% Created: 25-02-2013
+%% Description: TODO: Add description to email_quota
+-module(email_quota,[Id,
+                     EmailUsersId,
+                     RuleOrder,
+                     Rule,
+                     ClientId]).
+-belongs_to(email_users).
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
