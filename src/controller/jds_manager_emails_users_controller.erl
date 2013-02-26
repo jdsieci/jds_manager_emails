5803711dbcc2332c0339c3d851c24264f0595e8e
Jerzy Drozdz <jerzy.drozdz@jdsieci.pl>
2013-02-26 15:34:02 +0100

    add controller

--- src/controller/jds_manager_emails_users_controller.erl
+++ src/controller/jds_manager_emails_users_controller.erl
@@ -12,19 +12,32 @@
 %%
 %% Exported Functions
 %%
--export([list/2]).
+-export([list/2, add/2]).
 
 %%
 %% API Functions
 %%
 list('GET', []) ->
     Users = boss_db:find(email_users, []),
-    {json, [{users, Users}]};
+    {ok, [{users, Users}]};
 list('GET', [Domain]) ->
     {ok, #email_domains{id=DomainId}} = boss_db:find(email_domains, [{name, 'equals', Domain}]),
     Users = boss_db:find(email_users, [{email_domains_id, 'equals', DomainId}]),
-    {json, [{users, Users}]}.
+    {ok, [{users, Users}]}.
 
+add('GET', []) ->
+    Domains = boss_db:find(email_domains, []),
+    {ok, [{domains, Domains}]};
+add('GET', [Domain]) ->
+    Domains = boss_db:find(email_domains, [{name, 'equals', Domain}]),
+    {ok, [{domains, Domains}]};
+add('POST', []) ->
+    {redirect, {action, "list"}};
+add('POST', [Domain]) ->
+    {redirect, [{action, "list"}, {domain, Domain}]}.
+
+del('POST', []) ->
+    {redirect, {action, "list"}}.
 %%
 %% Local Functions
 %%
